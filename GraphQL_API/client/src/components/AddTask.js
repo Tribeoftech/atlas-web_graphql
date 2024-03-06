92e116ed534491c293a916e2bd7a602bdc47b239
Tribeoftech <gratefulgrowing333@gmail.com>
2024-03-06 11:29:52 -0600

    updated port for 3030

--- GraphQL_API/client/src/components/AddTask.js
+++ GraphQL_API/client/src/components/AddTask.js
@@ -1,97 +1,41 @@
-import {
-  useState,
-  //useEffect
-} from "react";
+/**
+ * AddTask component fetches projects via the getProjectsQuery and displays them
+ * in a select dropdown. Allows user to select a project when adding a new task.
+ */
+import { gql } from "apollo-boost";
+import React from "react";
+import { graphql } from "react-apollo";
 
+const getProjectsQuery = gql`
+  {
+    projects {
+      id
+      title
+    }
+  }
+`;
 
 function AddTask(props) {
-  const [inputs, setInputs] = useState({
-    title: '',
-    weight: 1,
-    description: '',
-    projectId: ''
-  });
+  function displayProjects() {
+    const { loading, projects } = props.data;
 
-
-  const handleChange = (e) => {
-        const newInputs = {
-          ...inputs
-        };
-        if (e.target.name === "weight") newInputs[e.target.name] = parseInt(e.target.value)
-        else newInputs[e.target.name] = e.target.value
-        setInputs(newInputs)
+    if (loading) {
+      return <option>Loading projects...</option>;
+    } else {
+      return projects.map((project) => (
+        <option key={project.id} value={project.id}>
+          {project.title}
+        </option>
+      ));
+    }
   }
 
-  return ( <
-    form class = "task"
-    id = "add-task"
-    /*onSubmit = {...}*/ >
-    <
-    div className = "field" >
-    <
-    label > Task title: < /label> <
-    input type = "text"
-    name = "title"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.title
-    }
-    required /
-    >
-    < /
-    div > <
-    div className = "field" >
-    <
-    label > Weight: < /label> <
-    input type = "number"
-    name = "weight"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.weight
-    }
-    required /
-    >
-    < /
-    div >
-    <
-    div className = "field" >
-    <
-    label > description: < /label> <
-    textarea name = "description"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.description
-    }
-    required /
-    >
-    < /
-    div >
-    <
-    div className = "field" >
-    <
-    label > Project: < /label> <
-    select name = "projectId"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.projectId
-    }
-    required > < option value = ""
-    selected = "selected"
-    disabled = "disabled" > Select project < /option>  < /
-    select > < /
-    div >
-    <
-    button > + < /button> < /
-    form >
+  return (
+    <select>
+      <option>Select Project</option>
+      {displayProjects()}
+    </select>
   );
 }
 
-export default AddTask;
+export default graphql(getProjectsQuery)(AddTask);
