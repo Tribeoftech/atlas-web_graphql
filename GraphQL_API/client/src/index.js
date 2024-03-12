d9af77ce5d2b0fecce8dbdf9b9d13927f69de3dd
Tribeoftech <gratefulgrowing333@gmail.com>
2024-03-12 14:14:23 -0500

    json

--- GraphQL_API/client/src/index.js
+++ GraphQL_API/client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './client/src/App';
+import './client/src/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
