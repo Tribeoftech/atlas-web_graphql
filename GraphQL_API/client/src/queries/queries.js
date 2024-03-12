d769cd7d711a5031c5a5e7e119d4eccca3d3f91e
Tribeoftech <gratefulgrowing333@gmail.com>
2024-03-12 14:29:16 -0500

    queries

---
+++ GraphQL_API/client/src/queries/queries.js
@@ -0,0 +1,47 @@
+import { gql } from 'apollo-boost';
+
+
+const getProjectsQuery = gql`
+  {
+    projects {
+      id
+      title
+    }
+  }
+`
+
+const getTasksQuery = gql`
+  {
+    tasks {
+      id
+      title
+      weight
+      description
+      projectId
+    }
+  }
+`
+
+const getTaskQuery = gql`
+  query($id: ID) {
+    task(id:$id) {
+      id
+      title
+      weight
+      description
+      project{
+        id
+        title
+        weight
+        description
+        tasks{
+          title
+          id
+          weight
+        }
+      }
+    }
+  }
+`
+
+export { getProjectsQuery, getTaskQuery, getTasksQuery };
